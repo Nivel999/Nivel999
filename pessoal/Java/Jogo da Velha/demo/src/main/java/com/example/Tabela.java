01940cec0f28f1481573ed8f758e71ff88fb9a43
Davi Oli <davioliveira1737@gmail.com>
2026-06-17 18:21:39 -0300

    Mudanças

--- pessoal/Java/Jogo da Velha/demo/src/main/java/com/example/Tabela.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-public class Tabela {
-   
-    public String Tabela(char[][] tab, int linha, int coluna) {
-        return "|" + tab[linha][coluna] + "|" + tab[linha][coluna] + "|" + tab[linha][coluna] + "|";
-    }
-}
